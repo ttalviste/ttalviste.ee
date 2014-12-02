c66f740fe5092934064da47516f4f15ffb5a9b1c
Toomas Talviste <toomas.talviste@fcrmedia.com>
2014-12-02 16:04:23 +0200

    Add GA, Add Flush;

--- src/ttalviste.common/Configuration.cs
+++ src/ttalviste.common/Configuration.cs
@@ -5,5 +5,13 @@ namespace ttalviste.common
     public class Configuration
     {
         public static string WebSiteName { get { return "ttalviste"; } }
+        public static string ProductionGoogeAnalytics
+        {
+            get { return "UA-41092859-2"; }
+        }
+        public static string DevelopmentGoogeAnalytics
+        {
+            get { return "UA-41092859-3"; }
+        }
     }
 }
--- src/ttalviste.web/Controllers/HomeController.cs
+++ src/ttalviste.web/Controllers/HomeController.cs
@@ -1,7 +1,19 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Razor;
 using ttalviste.web.Models.Home;
+using System.Threading;
+
 namespace ttalviste.web.Controllers
 {
+    public class RazorTest : RazorPage
+    {
+        public override Task ExecuteAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
     public class HomeController : Controller
     {
         public IActionResult Index()
